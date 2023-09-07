db7519959acd365c84905cb95b5c1a7cf650eddb
abnercezar <102832541+abnercezar@users.noreply.github.com>
2023-09-07 02:31:07 +0000

    Wed, Sep 6, 2023, 11:31 PM -03:00

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -178,7 +178,7 @@ def minimax(board):
     elif player(board) == X:
         for action in actions(board):
             plays.append([min_value(result(board, action)), action])
-        return sorted(plays, key=lamba x: x[0], reverse=True)[0][1]
+        return sorted(plays, key=lambda x: x[0], reverse=True)[0][1]
 
      elif player(board) == O:
         for action in actions(board):
