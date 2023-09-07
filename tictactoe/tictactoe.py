b2432aab916d76b66972b26db27f5a0d70207758
abnercezar <102832541+abnercezar@users.noreply.github.com>
2023-09-07 02:32:12 +0000

    Wed, Sep 6, 2023, 11:32 PM -03:00

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -183,4 +183,4 @@ def minimax(board):
      elif player(board) == O:
         for action in actions(board):
             plays.append([max_value(result(board, action)), action])
-        return sorted(plays, key=lamba x: x[0])[0][1]
+        return sorted(plays, key=lambda x: x[0])[0][1]
