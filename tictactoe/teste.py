770aa4dbee0b429cbfcc0e243ec7181c4a10dce2
abnercezar <102832541+abnercezar@users.noreply.github.com>
2023-09-16 00:24:25 +0000

    Fri, Sep 15, 2023, 9:24 PM -03:00

--- tictactoe/teste.py
+++ tictactoe/teste.py
@@ -4,7 +4,7 @@ screen = pygame.display.set_mode((640, 480))
 running = True
 while running:
     for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-        pygame.display.flip()
-    pygame.quit()
+        if event.type == pygame.QUIT:
+            running = False
+    pygame.display.flip()
+pygame.quit()
