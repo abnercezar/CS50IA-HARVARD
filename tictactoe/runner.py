a26d5db5843c10bedbcad3a4e051ab848d6c7f8d
abnercezar <102832541+abnercezar@users.noreply.github.com>
2023-09-17 13:48:34 +0000

    Sun, Sep 17, 2023, 10:48 AM -03:00

--- tictactoe/runner.py
+++ tictactoe/runner.py
@@ -1,4 +1,4 @@
-""" import pygame
+import pygame
 import sys
 import time
 
@@ -145,21 +145,3 @@ while True:
                     ai_turn = False
 
     pygame.display.flip()
- """
-
-import pygame
-pygame.init()
-
-win = pygame.display.set_mode((500, 500))
-
-run = True
-while run:
-    pygame.time.delay(100)
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-
-    pygame.display.update()
-
-pygame.quit()
