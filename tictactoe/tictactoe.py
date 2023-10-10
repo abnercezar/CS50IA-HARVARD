961e2f869f3ae123d81882ba2942865f10b5708f
abnercezar <102832541+abnercezar@users.noreply.github.com>
2023-10-10 01:46:48 +0000

    Mon, Oct 9, 2023, 10:46 PM -03:00

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -68,6 +68,8 @@ def terminal(board):
     if is_board_full(board):
         return True
     return False
+if check_win(board, player(board)):
+    return [((i, j), float("infinity"))]  # winning move gets maximum score
 
 
 # A utilityfunção deve aceitar um terminal boardcomo entrada e saída da utilidade da placa.
