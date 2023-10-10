68d89dc17f3339c1b3640d9e50bc141970545483
abnercezar <102832541+abnercezar@users.noreply.github.com>
2023-10-10 01:18:42 +0000

    Mon, Oct 9, 2023, 10:18 PM -03:00

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -105,6 +105,27 @@ def minimax(board):
                     best_move.append((i, j))
     return random.choice(best_move)
 
+function minimax(board, depth, isMaximizingPlayer):
+    if game is over or depth is 0:
+        return static evaluation of board
+
+    if isMaximizingPlayer:
+        bestScore = -infinity
+        for each move:
+            make move
+            score = minimax(board, depth - 1, false)
+            undo move
+            bestScore = max(score, bestScore)
+        return bestScore
+    else:
+        bestScore = +infinity
+        for each move:
+            make move
+            score = minimax(board, depth - 1, true)
+            undo move
+            bestScore = min(score, bestScore)
+        return bestScore
+
 
 possible_moves = None
 
