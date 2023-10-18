b36546cc4edab353c1a7e5ad42a5062d07b2b63f
abnercezar <102832541+abnercezar@users.noreply.github.com>
2023-10-18 02:25:20 +0000

    Tue, Oct 17, 2023, 11:25 PM -03:00

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -140,7 +140,48 @@ def min_value(board):
     return value
 
 
+# Esta função verifica se um jogador ganhou preenchendo uma linha inteira no 'tabuleiro'.
+def checkRows(board, player):
+    for row in range(len(board)):
+        if (
+            board[row][0] == player
+            and board[row][1] == player
+            and board[row][2] == player
+        ):
+            return True
+    return False
 
 
+# Esta função verifica se um jogador ganhou preenchendo as colunas
+def checkColumns(board, player):
+    for col in range(len(board)):
+        if (
+            board[0][col] == player
+            and board[1][col] == player
+            and board[2][col] == player
+        ):
+            return True
+    return False
 
 
+# Esta função verifica se um jogador ganhou preenchendo as diagonais do 'tabuleiro'.
+# Verifique a diagonal principal
+def checkFirstDiag(board, player):
+    count = 0
+    for row in range(len(board)):
+        if board[row][row] == player:
+            count += 1
+    if count == 3:
+        return True
+    return False
+
+
+# Verifique a diagonal secundária
+def checkSecDiag(board, player):
+    count = 0
+    for row in range(len(board)):
+        if board[row][2 - row] == player:
+            count += 1
+    if count == 3:
+        return True
+    return False
