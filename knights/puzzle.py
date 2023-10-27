66564d0d3e65959fbc69dd6da61b8f205c33e8dd
abnercezar <102832541+abnercezar@users.noreply.github.com>
2023-10-27 02:02:22 +0000

    Thu, Oct 26, 2023, 11:02 PM -03:00

--- knights/puzzle.py
+++ knights/puzzle.py
@@ -59,7 +59,7 @@ knowledge3 = And(
     Implication(AKnight, Or(AKnight, AKnave)),
     Implication(AKnave, Not(Or(AKnight, AKnave))),
 
-    Or(Implication(BKnight, Or(Implication(AKnight, AKnave), Implication(AKnave, Not(AKnave)))), Implication(not BKnave(Not(Or())))),
+    Or(Implication(BKnight, Or(Implication(AKnight, AKnave), Implication(AKnave, Not(AKnave)))), Implication(not BKnave, Not(Or())))),
 
     Implication(BKnight, CKnave),
     Implication(BKnave, Not(CKnave)),
